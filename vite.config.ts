cf65f047215a573076b4b7c27090fc18395f84e7
NucleoFusion <lakshit.singh.mail@gmail.com>
2025-09-17 17:12:25 +0530

    fix
    Signed-off-by: NucleoFusion <lakshit.singh.mail@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,6 @@ import { defineConfig } from 'vite';
 import svg from "@poppanator/sveltekit-svg";
 
 export default defineConfig({
-	server: {
-		host: true,
-		allowedHosts: true,
-		port: 5173,
-	},
 	plugins: [
 		tailwindcss(),
 		sveltekit(),
